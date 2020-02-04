30a27eb0f2a2f6979e11151b7afcca1091c6ea40
최석훈 <koon0902@gmail.com>
2020-02-04 21:07:48 +0900

    home local commit

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var gentleman = require('./api/gentleman');
 
 const app = express();
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 app.use(function(err, req, res, next){
     if(err.name === 'StatusError'){
