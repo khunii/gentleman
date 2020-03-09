a897c615bacf3d9808c3ab62c34b9ee240e1baad
최석훈 <koon0902@gmail.com>
2020-03-09 16:18:37 +0900

    add startScript

---
+++ startGentleman.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROCESS='www.js'
+PID=`ps -ef | grep ${PROCESS} | grep -v grep | awk '{print $2}'`
+workspace='gentleman'
+echo "node process ID : $PID"
+if [ -z "$PID" ]; then
+echo "No process is running"
+else
+echo "Kill process"
+kill -9 $PID
+kill -9 `ps -ef | grep ${workspace} | grep -v grep | awk '{print $2}'`
+fi
